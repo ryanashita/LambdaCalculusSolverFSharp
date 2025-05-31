153eab59d3e810419c920b79d8c0a3cfd7da858d
ryanashita <ryunosukeyanashita@gmail.com>
2025-05-31 10:14:56 -0700

    change to sln

---
+++ code/tests/Test1.fs
@@ -0,0 +1,25 @@
+﻿namespace tests
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Parser
+open AST
+
+[<TestClass>]
+type Test1 () =
+
+    [<TestMethod>]
+    member this.TestMethodPassing () =
+        Assert.IsTrue(true);
+
+    [<TestMethod>]
+    member this.SimpleParserTest () = 
+        let input = "Lx.xy"
+        let result = parse input
+        match result with
+        | Some actual -> 
+            let expected = Abstraction ('x', Application (Variable 'x', Variable 'y'))
+            let is_same = expected = actual
+            Assert.IsTrue is_same
+        | None -> Assert.IsTrue false
+
